e01bf9194397c2976ce77b5d909ffdead887cd33
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-05-01 17:21:05 -0300

    mais ou menos mais ou menos

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="informações" />
+        <h2>Nossa Historioa</h2>
+        <p>ROlanafslkfja alkjfaia fspaodsjfas fda</p>
+        <h2>Nossa missão</h2>
+        <p>LAJfaosf oajfaosfja psjfaspf asfa</p>
+        <h2>Contato</h2>
+        <p>leonardo.ncintra@OUtlook.com</p>
     </div>
 )
