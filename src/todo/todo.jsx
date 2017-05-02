cd3e9ef389f59fc0b0e27c15cd4036749a2e4229
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-05-01 22:07:06 -0300

    add e exclusao ok

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -17,16 +17,34 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createAt`)
+            .then(resp => this.setState({
+                ...this.state, 
+                description: '',
+                list: resp.data
+            }))
     }
 
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
     }
 
+    // Adicionar tarefa
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou'))
+            .then(resp => this.refresh())
+    }
+
+    // Remover tarefa
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
     
     render() {
@@ -37,7 +55,9 @@ export default class Todo extends Component {
                     description={ this.state.description }
                     handleChange={ this.handleChange }
                     handleAdd={ this.handleAdd } />
-                <TodoList />
+                <TodoList 
+                    list={ this.state.list }
+                    handleRemove={ this.handleRemove } />
             </div>
         )
     }
